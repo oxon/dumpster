5c9454b1536ed3278a146304b8935884907f59d1
Nicolas Schaefer <ns@oxon.ch>
2016-02-11 14:13:01 +0100

    Option um use_tempfile erweitert

--- lib/dumpster/excel.rb
+++ lib/dumpster/excel.rb
@@ -14,18 +14,37 @@ module Dumpster
       end
 
       def write_to_file(path, options = {})
-        options = { autowidth: true, trust_input: false, shared_strings: false }.merge(options)
+        options = { autowidth: true, trust_input: false, shared_strings: false, use_tempfile: false }.merge(options)
         autowidth = options.delete(:autowidth)
         trust_input = options.delete(:trust_input)
         shared_strings = options.delete(:shared_strings)
+        use_tempfile = options.delete(:use_tempfile)
         raise "unexpected options: #{options}" unless options.empty?
 
         Axlsx::trust_input = trust_input
         package = Axlsx::Package.new
         package.use_autowidth = autowidth
         package.use_shared_strings = shared_strings
+        package.use_tempfile = use_tempfile
         draw(package)
         package.serialize(path)
+      end
+
+      def write_to_stream(options = {})
+        options = { autowidth: true, trust_input: false, shared_strings: false, use_tempfile: false }.merge(options)
+        autowidth = options.delete(:autowidth)
+        trust_input = options.delete(:trust_input)
+        shared_strings = options.delete(:shared_strings)
+        use_tempfile = options.delete(:use_tempfile)
+        raise "unexpected options: #{options}" unless options.empty?
+
+        Axlsx::trust_input = trust_input
+        package = Axlsx::Package.new
+        package.use_autowidth = autowidth
+        package.use_shared_strings = shared_strings
+        package.use_tempfile = use_tempfile
+        draw(package)
+        package.to_stream
       end
 
       def draw(package)
